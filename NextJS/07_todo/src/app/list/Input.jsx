2b35c9ee7e67c7917e4bb1602544376df381dd1e
kim <zer0box@naver.com>
2025-04-21 12:57:54 +0900

    Input 기능 추가

--- NextJS/07_todo/src/app/list/Input.jsx
+++ NextJS/07_todo/src/app/list/Input.jsx
@@ -1,12 +1,29 @@
 import InputStyle from "./Input.module.css"
+import {useState} from "react";
+import axios from "axios";
 
 export default function Input({id}){
+
+    const [content, setContent] = useState();
+
+    const send = async()=>{
+        let param = {"id":id, "content":content,"done":false};
+        console.log(param);
+        let {data} = await axios.post('http://localhost/insert',param);
+        console.log(data);
+        if(!data.loginYN){
+            alert('로그인이 필요한 서비스 입니다.');
+            location.href='/';
+        }
+
+    }
 
     return(
         <div>
             <h2>해야할 일</h2>
-            <input type="text" value={""} className={InputStyle.inputBox}/>
-            <button className={InputStyle.button}>추가</button>
+            <input type="text" value={content} className={InputStyle.inputBox}
+                   onChange={(e)=>{setContent(e.target.value)}}/>
+            <button className={InputStyle.button} onClick={send}>추가</button>
         </div>
     );
 }
