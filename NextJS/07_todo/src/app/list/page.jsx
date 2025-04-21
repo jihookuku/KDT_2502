f3e1753eada5c967303b04eb5c0409e57b1fc9d8
kim <zer0box@naver.com>
2025-04-21 11:50:32 +0900

    증복 로그 처리 끔, sessionStorage 에서 아이디 가져오기

--- NextJS/07_todo/src/app/list/page.jsx
+++ NextJS/07_todo/src/app/list/page.jsx
@@ -1,4 +1,13 @@
+'use client'
 export default function ListPage(props) {
+
+    let id = ''
+
+    if(typeof window !== 'undefined') {
+        id = sessionStorage.getItem('loginId');
+        console.log('loginId',id);
+    }
+
     return (
         <>
             <h3>INPUT</h3>
