5393c50bb7acd8d3e7fb0a9b5761d493ee969860
kim <zer0box@naver.com>
2025-04-21 14:00:12 +0900

    Input 기능 완성

--- NextJS/07_todo/src/app/list/Input.jsx
+++ NextJS/07_todo/src/app/list/Input.jsx
@@ -2,9 +2,9 @@ import InputStyle from "./Input.module.css"
 import {useState} from "react";
 import axios from "axios";
 
-export default function Input({id}){
+export default function Input({id, getList}){
 
-    const [content, setContent] = useState();
+    const [content, setContent] = useState('');
 
     const send = async()=>{
         let param = {"id":id, "content":content,"done":false};
@@ -15,14 +15,26 @@ export default function Input({id}){
             alert('로그인이 필요한 서비스 입니다.');
             location.href='/';
         }
+        if(data.success){
+            alert('추가 하였습니다.');
+            setContent('');
+            getList(id);
+        }else{
+            alert('추가에 실패 하였습니다.');
+        }
+    }
 
+    const keyHandler = (e)=>{
+        if(e.keyCode == 13){
+            send();
+        }
     }
 
     return(
         <div>
             <h2>해야할 일</h2>
             <input type="text" value={content} className={InputStyle.inputBox}
-                   onChange={(e)=>{setContent(e.target.value)}}/>
+                   onChange={(e)=>{setContent(e.target.value)}} onKeyUp={keyHandler}/>
             <button className={InputStyle.button} onClick={send}>추가</button>
         </div>
     );
--- NextJS/07_todo/src/app/list/page.jsx
+++ NextJS/07_todo/src/app/list/page.jsx
@@ -1,18 +1,35 @@
 'use client'
 import Input from "@/app/list/Input";
+import {useEffect, useRef, useState} from "react";
+import axios from "axios";
 
 export default function ListPage(props) {
 
-    let id = '';
+    const [list,setList] = useState([]);
+    const id = useRef('');
 
-    if(typeof window !== 'undefined') {
-        id = sessionStorage.getItem('loginId');
-        console.log('loginId',id);
+    useEffect(() => {
+        if(typeof window !== 'undefined') {
+            id.current = sessionStorage.getItem('loginId');
+
+        }
+        getList(id.current);
+    },[]);
+
+    const getList = async(loginId) => {
+        console.log(loginId+' list 호출 실행');
+        let {data} = await axios.post('http://localhost/list',{"id":loginId});
+        console.log(data);
+        if(!data.loginYN){
+            alert('로그인이 필요한 서비스 입니다.');
+            location.href='/';
+        }
+        setList(data.list);
     }
 
     return (
         <>
-            <Input id={id}/>
+            <Input id={id.current} getList={getList}/>
             <h3>LIST</h3>
         </>
     );
