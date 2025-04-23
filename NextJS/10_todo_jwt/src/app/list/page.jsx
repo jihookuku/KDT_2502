b73cda82db87fc1795462b1b6c5be5db3c4458d7
kim <zer0box@naver.com>
2025-04-23 10:52:40 +0900

    로그인 기능 완성

---
+++ NextJS/10_todo_jwt/src/app/list/page.jsx
@@ -0,0 +1,9 @@
+export default function TodoPage(){
+    return(
+        <>
+            <h3>해야 할 일</h3>
+            <div>INPUT</div>
+            <div>LIST</div>
+        </>
+    );
+}
--- NextJS/10_todo_jwt/src/app/page.jsx
+++ NextJS/10_todo_jwt/src/app/page.jsx
@@ -1,10 +1,32 @@
 // login page -> "/"
 // join page -> "/join"
 // todo-list page -> "/list"
+'use client'
 import "./member.css"
 import Link from "next/link";
+import axios from "axios";
+import {useState} from "react";
 
 export default function LoginPage(){
+
+    const [info, setInfo] = useState({id:'',pw:''});
+
+    const input=(e) => {
+        let {name, value} = e.target;
+        setInfo({...info,[name]:value});
+    }
+
+    const login = async () => {
+        let {data}=await axios.post('http://localhost/login',info);
+        if(data.success){
+            alert('로그인에 성공 했습니다.');
+            sessionStorage.setItem('token',data.token);
+            location.href='/list';
+        }else{
+            alert('아이디 또는 비밀번호를 확인하세요!');
+        }
+    }
+
     return(
         <>
             <h3>로그인</h3>
@@ -12,16 +34,15 @@ export default function LoginPage(){
                 <tbody>
                     <tr>
                         <th>ID</th>
-                        <td><input type="text" name="id"/></td>
+                        <td><input type="text" name="id" onChange={input} value={info.id}/></td>
                     </tr>
                     <tr>
                         <th>PW</th>
-                        <td><input type="password" name="pw"/></td>
+                        <td><input type="password" name="pw" onChange={input} value={info.pw}/></td>
                     </tr>
-
                     <tr>
                         <th colSpan="2">
-                            <button>LOGIN</button>
+                            <button onClick={login}>LOGIN</button>
                             <Link href="/join">회원가입</Link>
                         </th>
                     </tr>
