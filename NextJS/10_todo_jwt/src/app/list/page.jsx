3fb73c70eafab65e6e42985a14b72c8bcd092e78
kim <zer0box@naver.com>
2025-04-23 12:47:29 +0900

    list 불러오기

--- NextJS/10_todo_jwt/src/app/list/page.jsx
+++ NextJS/10_todo_jwt/src/app/list/page.jsx
@@ -1,8 +1,25 @@
 'use client'
 import Input from "@/app/list/Input";
 import "./todo.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function TodoPage(){
+
+    const[list,setList]=useState([]);
+
+    useEffect(()=>{
+        getList();
+    },[]);
+
+    const getList=async()=>{
+        const token = sessionStorage.getItem("token");
+        const loginId = sessionStorage.getItem("loginId");
+        const {data} = await axios.post('http://localhost/list',{"id":loginId}, {headers:{authorization: token}})
+        console.log(data);
+        setList([...list,data.list]);
+    }
+
     return(
         <>
             <h3>해야 할 일</h3>
