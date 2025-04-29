3a6c0e1ef940e482de3a9f3916892fc57378f5c6
kim <zer0box@naver.com>
2025-04-29 15:38:17 +0900

    redux/tool 을 이용한 counter 앱

--- NextJS/17_count/src/app/page.jsx
+++ NextJS/17_count/src/app/page.jsx
@@ -1,7 +1,27 @@
+'use client'
+import {store} from "@/redux/store";
+import {useSelector} from "react-redux";
+
 export default function HomePage(){
+
+    const countUp = () => {
+       store.dispatch({type:'counter/increment', payload:'매개변수'});
+    };
+
+    const countDn = () => {
+        store.dispatch({type:'counter/decrement', payload:'매개변수'});
+    };
+
+    let cnt = useSelector(state => { // store 에 등록된 모든 slice의 state 정보
+        console.log(state);
+        return state.counter.count;
+    });
+
     return (
         <div>
-            
+            <h3>COUNT : {cnt}</h3>
+            <button onClick={countUp}>증가</button>
+            <button onClick={countDn}>감소</button>
         </div>
     );
 }
