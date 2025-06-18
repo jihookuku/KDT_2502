9a208144c6e8e5b69d73d7b5fc554aa0a3128ad2
kim <zer0box@naver.com>
2025-06-18 13:14:22 +0900

    서비스 완성

--- JPA/04_custom/src/main/java/kr/co/himedia/service/CustomService.java
+++ JPA/04_custom/src/main/java/kr/co/himedia/service/CustomService.java
@@ -1,10 +1,13 @@
 package kr.co.himedia.service;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.co.himedia.entity.Employee;
 import kr.co.himedia.repo.EmployeeRepo;
 
 @Service
@@ -13,5 +16,44 @@ public class CustomService {
 	Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@Autowired EmployeeRepo empRepo;
+	
+	public List<Employee> findByName(String name){
+		return empRepo.findByName(name);
+	}
+	
+	public List<Employee> findByAgeOrderByNoAsc(int age){
+		return empRepo.findByAgeOrderByNoAsc(age);
+	}
+	
+	public List<Employee> findByAgeAndName(int age, String name){
+		return empRepo.findByAgeAndName(age, name);
+	}
+	
+	public List<Employee> findByNameAndEmail(String name, String email){
+		return findByNameAndEmail(name, email);
+	}
+	
+	public List<Employee> findByNameOrEmailOrderByNoDesc(String name, String email){
+		return findByNameOrEmailOrderByNoDesc(name, email);
+	}
+	
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
