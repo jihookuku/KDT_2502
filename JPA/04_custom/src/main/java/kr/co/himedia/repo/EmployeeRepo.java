d61a98f7ead3ff42be5157cb107e4fa199e18d41
kim <zer0box@naver.com>
2025-06-18 13:01:56 +0900

    쿼리문

--- JPA/04_custom/src/main/java/kr/co/himedia/repo/EmployeeRepo.java
+++ JPA/04_custom/src/main/java/kr/co/himedia/repo/EmployeeRepo.java
@@ -5,5 +5,20 @@ import org.springframework.data.repository.CrudRepository;
 import kr.co.himedia.entity.Employee;
 
 public interface EmployeeRepo extends CrudRepository<Employee, Integer> {
+	
+	// SELECT * FROM employee WHERE name = #{name}
+	
+	// SELECT * FROM employee WHERE age = #{age} ORDER BY no ASC
+	
+	// SELECT * FROM employee WHERE age = #{age} AND name = #{name}
+	
+	// SELECT * FROM employee WHERE name = #{name} AND email = #{email}
+	
+	// SELECT * FROM employee WHERE name = #{name} OR email = #{email} ORDER BY no DESC
+	
+	
+	
+	
+	
 
 }
