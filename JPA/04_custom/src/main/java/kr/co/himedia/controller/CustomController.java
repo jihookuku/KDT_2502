d42985d437645be6d4425c4979eac5b7862ef5b8
kim <zer0box@naver.com>
2025-06-18 14:38:32 +0900

    findByName

--- JPA/04_custom/src/main/java/kr/co/himedia/controller/CustomController.java
+++ JPA/04_custom/src/main/java/kr/co/himedia/controller/CustomController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,35 @@ public class CustomController {
 	public List<Employee> inputList(@RequestBody List<Employee> list){
 		return service.saveAll(list);
 	}
+	
+	@GetMapping(value="/find/name/{name}")
+	public List<Employee> fineByName(@PathVariable String name){
+		logger.info("name="+name);
+		return service.findByName(name);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
