8224ffa14eb271454c349898e8140393d51ff34b
kim <zer0box@naver.com>
2025-06-19 16:57:50 +0900

    사진 게시판 초기 프로젝트 설정

---
+++ JPA/09_PhotoBoard/src/main/java/kr/co/himedia/entity/Board.java
@@ -0,0 +1,71 @@
+package kr.co.himedia.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="bbs")
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idx;
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	private String subject;
+	
+	@Lob
+	@Column(columnDefinition = "longtext")
+	@Basic(fetch = FetchType.LAZY)
+	private String content;
+	
+	@Column(name="b_hit")
+	private long bHit;
+	
+	@CreationTimestamp // 테이블 생성시currdate 를 추가
+	private LocalDate reg_date;
+	
+	@OneToMany(mappedBy = "bbs", cascade = CascadeType.ALL)
+	private List<Photo> photos;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
