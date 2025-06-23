c1568689f570f594aa8ede29dbae92e3dce73e8e
kim <zer0box@naver.com>
2025-06-23 16:54:27 +0900

    암호화

--- JPA/13_Encoder/src/main/java/kr/co/himedia/config/SecurityConfig.java
+++ JPA/13_Encoder/src/main/java/kr/co/himedia/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package kr.co.himedia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,5 +15,19 @@ public class SecurityConfig {
 	public PasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	//스프링 시큐리티의 암호화 기능만을 사용하고 나머지는 모두 비활성화 해 준다.
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		//로그인 기본 설정을 비활성화
+		http.httpBasic().disable();		
+		return http.build();
+	}
 
 }
+
+
+
+
+
+
--- JPA/13_Encoder/src/main/java/kr/co/himedia/controller/EncodeController.java
+++ JPA/13_Encoder/src/main/java/kr/co/himedia/controller/EncodeController.java
@@ -25,6 +25,9 @@ public class EncodeController {
 		return "Hello Home Page";
 	}
 	
+	// 평문과 암화문의 패턴을 파악할수 없도록 같은 값을 넣어도 서로 다른 암호문이 출력되도록 한다.
+	// 이때 추가되는 값을 sault 라고 한다.
+	// 1234 -> 1234? -> 암호문
 	@GetMapping(value="/encode/{msg}")
 	public Map<String, String> encode(@PathVariable String msg) {
 		
@@ -36,5 +39,27 @@ public class EncodeController {
 		
 		return map;
 	}
+	
+	// 암호문은 평문이 입력될때마다 달라지므로 직접 비교가 불가능 하다.
+	// 평문 -> 암호화 -> 기존 함호문과 비교 이런식은 불가능하다.
+	// 그래서 spring security 가 제공하는 메서드를 통해서만 비교해야 한다.
+	@GetMapping(value="/match/{msg}")
+	public Map<String, Object> match(@PathVariable String msg){		
+		boolean success = encoder.matches(msg, hash);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("match", success);
+		return map;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
