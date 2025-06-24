d467400449bd9b0e41c65219b2a8d66b2e404b04
kim <zer0box@naver.com>
2025-06-24 16:04:46 +0900

    테스트 완료

--- JPA/15_Employees/src/main/java/kr/co/himedia/repo/TableRepo.java
+++ JPA/15_Employees/src/main/java/kr/co/himedia/repo/TableRepo.java
@@ -12,6 +12,12 @@ import kr.co.himedia.entity.Departments;
 import kr.co.himedia.entity.DeptEmp;
 import kr.co.himedia.entity.DeptManager;
 import kr.co.himedia.entity.Employees;
+import kr.co.himedia.entity.QDepartments;
+import kr.co.himedia.entity.QDeptEmp;
+import kr.co.himedia.entity.QDeptManager;
+import kr.co.himedia.entity.QEmployees;
+import kr.co.himedia.entity.QSalaries;
+import kr.co.himedia.entity.QTitles;
 import kr.co.himedia.entity.Salaries;
 import kr.co.himedia.entity.Titles;
 import lombok.extern.slf4j.Slf4j;
@@ -29,27 +35,33 @@ public class TableRepo {
 	}
 
 	public List<Employees> emp(int limit) {
-		return null;
+		QEmployees e = QEmployees.employees;		
+		return factory.selectFrom(e).limit(limit).fetch();
 	}
 
 	public List<Departments> dept(int limit) {
-		return null;
+		QDepartments d = QDepartments.departments;		
+		return factory.selectFrom(d).limit(limit).fetch();
 	}
 
 	public List<Titles> title(int limit) {	
-		return null;
+		QTitles t = QTitles.titles;		
+		return  factory.selectFrom(t).limit(limit).fetch();
 	}
 
 	public List<Salaries> salary(int limit) {
-		return null;
+		QSalaries s = QSalaries.salaries;
+		return  factory.selectFrom(s).limit(limit).fetch();
 	}
 
 	public List<DeptEmp> deptEmp(int limit) {
-		return null;
+		QDeptEmp de = QDeptEmp.deptEmp;
+		return factory.selectFrom(de).limit(limit).fetch();
 	}
 
 	public List<DeptManager> deptManager(int limit) {
-		return null;
+		QDeptManager dm = QDeptManager.deptManager;
+		return  factory.selectFrom(dm).limit(limit).fetch();
 	}
 	
 
